242c0b56199dc621e0a44d5c31ce04456587ac72
Ajit Kumar Gunturi <ajitg@vmware.com>
2024-03-05 22:09:40 +0530

    feat(footer): add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import UserDetails from './components/UserDetails';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <UserDetails/>      
+      <Footer/>
     </div>
+    
   );
 }
 
